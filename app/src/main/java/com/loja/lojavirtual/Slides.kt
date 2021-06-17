16b4a86e1146b0ca976ce52061477645b6ba272f
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2021-06-16 22:59:23 -0300

    Tela Inicial - SimpleSlide

--- app/src/main/java/com/loja/lojavirtual/Slides.kt
+++ app/src/main/java/com/loja/lojavirtual/Slides.kt
@@ -10,11 +10,14 @@ class Slides : IntroActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_slides) #Se fica descomentado, dará o efeito invisível
 
+        isButtonBackVisible = false
+
         addSlide(
 
             SimpleSlide.Builder()
                 .background(R.color.roxo)
                 .image(R.drawable.drawer)
+                .backgroundDark(R.color.branco)
                 .title("Loja Online de Calçados")
                 .description("Entre e confira as promoções que preparamos para você!")
                 .build()
