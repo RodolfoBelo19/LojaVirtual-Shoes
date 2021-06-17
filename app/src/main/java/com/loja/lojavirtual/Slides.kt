3996721a5f52f5498a3cb711fdf0e6d4d91c88a1
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2021-06-16 21:55:07 -0300

    First Commit

---
+++ app/src/main/java/com/loja/lojavirtual/Slides.kt
@@ -0,0 +1,32 @@
+package com.loja.lojavirtual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+
+class Slides : IntroActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_slides) #Se fica descomentado, dará o efeito invisível
+
+        addSlide(
+
+            SimpleSlide.Builder()
+                .background(R.color.roxo)
+                .image(R.drawable.drawer)
+                .title("Loja Online de Calçados")
+                .description("Entre e confira as promoções que preparamos para você!")
+                .build()
+        )
+
+        addSlide(
+
+            SimpleSlide.Builder()
+                .background(R.color.azul)
+                .title("Crie uma conta grátis")
+                .description("Cadastre-se agora mesmo! E venha conhecer os nossos produtos.")
+                .build()
+        )
+    }
+}
